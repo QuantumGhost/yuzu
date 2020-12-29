18774015b209c3b5fd7cb75bc90c882375addf9d
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2020-12-29 12:23:31 -0800

    Update docker.sh

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -2,7 +2,8 @@
 
 BRANCH=`echo ${GITHUB_REF##*/}`
 
-title=$(cat /yuzu/readme.md | grep 'early-access [[:digit:]]*' | cut -c 14-17)
+ver=$(cat /yuzu/README.md | grep 'early-access [[:digit:]]*' | cut -c 14-17)
+title="yuzu Early Access $ver"
 
 
 yuzupatch=( $(ls -d patches/* ) )
@@ -10,4 +11,12 @@ for i in "${yuzupatch[@]}"; do patch -p1 < "$i"; done
 
 mkdir build && cd build 
 
-ls .
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DTITLE_BAR_FORMAT_IDLE="$title" -DTITLE_BAR_FORMAT_RUNNING="$title | {3}" -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DGIT_BRANCH="HEAD" -DGIT_DESC="$msvc" -DUSE_DISCORD_PRESENCE=ON
+
+ninja
+
+
+cd /tmp
+curl -sLO "https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/.github/workflows/appimage.sh"
+chmod a+x appimage.sh
+./appimage.sh
