cd07202a1a24081dd5fd0bd71984d11441c5daf0
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-02-14 21:52:39 -0800

    remove code merged into src
    remove freezing fix
    remove keys child window
    add -DBUILD_DATE="$build_date"

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -14,14 +14,22 @@ find . -name "CMakeLists.txt" -exec sed -i 's/^.*-Werror=.*)$/ )/g' {} +
 find . -name "CMakeLists.txt" -exec sed -i 's/^.*-Werror=.*$/ /g' {} +
 find . -name "CMakeLists.txt" -exec sed -i 's/-Werror/-W/g' {} +
 
-#Fix freezing when closing the window
-sed -i -e '0,/is_thread_exiting.store(true);/! s/is_thread_exiting.store(true);/&\n    cv.notify_all();/' src/video_core/shader/async_shaders.cpp
-#Fix Deriving Keys window not being a child of main
-sed -i -e 's/QProgressDialog prog;/QProgressDialog prog(this);/g' src/yuzu/main.cpp
-
 mkdir build && cd build 
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DTITLE_BAR_FORMAT_IDLE="$title" -DTITLE_BAR_FORMAT_RUNNING="$title | {3}" -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DGIT_BRANCH="HEAD" -DGIT_DESC="$msvc" -DUSE_DISCORD_PRESENCE=ON -DENABLE_QT_TRANSLATION=ON
+cmake ..                                    \
+  -DCMAKE_BUILD_TYPE=Release                \
+  -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc    \ 
+  -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++  \
+  -DTITLE_BAR_FORMAT_IDLE="$title"          \
+  -DTITLE_BAR_FORMAT_RUNNING="$title | {3}" \
+  -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON   \
+  -DGIT_BRANCH="HEAD"                       \
+  -DGIT_DESC="$msvc"                        \
+  -DUSE_DISCORD_PRESENCE=ON                 \
+  -DENABLE_QT_TRANSLATION=ON                \
+  -DBUILD_DATE="$build_date"                \
+  -DYUZU_USE_QT_WEB_ENGINE=OFF              \
+  -G Ninja 
 
 ninja
 
