ae5276503a1a476af5a141ad9e1cea2df662e78b
pineappleEA <pineaea@gmail.com>
2021-02-28 23:30:20 +0100

    early-access version 1490

--- src/video_core/renderer_opengl/gl_texture_cache.cpp
+++ src/video_core/renderer_opengl/gl_texture_cache.cpp
@@ -568,12 +568,13 @@ void TextureCacheRuntime::BlitFramebuffer(Framebuffer* dst, Framebuffer* src,
 
 void TextureCacheRuntime::AccelerateImageUpload(Image& image, const ImageBufferMap& map,
                                                 std::span<const SwizzleParameters> swizzles) {
-    if (IsPixelFormatASTC(image.info.format)) {
-        return util_shaders.ASTCDecode(image, map, swizzles);
-    }
     switch (image.info.type) {
     case ImageType::e2D:
-        return util_shaders.BlockLinearUpload2D(image, map, swizzles);
+        if (IsPixelFormatASTC(image.info.format)) {
+            return util_shaders.ASTCDecode(image, map, swizzles);
+        } else {
+            return util_shaders.BlockLinearUpload2D(image, map, swizzles);
+        }
     case ImageType::e3D:
         return util_shaders.BlockLinearUpload3D(image, map, swizzles);
     case ImageType::Linear:
