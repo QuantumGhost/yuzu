6924d51f6d6ea2b0cd615946363ef16fc48124ed
pineappleEA <pineaea@gmail.com>
2021-06-20 20:27:37 +0200

    early-access version 1798

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 yuzu emulator early access
 =============
 
-This is the source code for early-access 1797.
+This is the source code for early-access 1798.
 
 ## Legal Notice
 
--- src/audio_core/audio_renderer.h
+++ src/audio_core/audio_renderer.h
@@ -6,6 +6,7 @@
 
 #include <array>
 #include <memory>
+#include <mutex>
 #include <vector>
 
 #include "audio_core/behavior_info.h"
@@ -33,6 +34,7 @@ class Memory;
 
 namespace AudioCore {
 using DSPStateHolder = std::array<VoiceState*, AudioCommon::MAX_CHANNEL_COUNT>;
+constexpr s32 NUM_BUFFERS = 2;
 
 class AudioOut;
 
@@ -45,6 +47,8 @@ public:
 
     [[nodiscard]] ResultCode UpdateAudioRenderer(const std::vector<u8>& input_params,
                                                  std::vector<u8>& output_params);
+    [[nodiscard]] ResultCode Start();
+    [[nodiscard]] ResultCode Stop();
     void QueueMixedBuffer(Buffer::Tag tag);
     void ReleaseAndQueueBuffers();
     [[nodiscard]] u32 GetSampleRate() const;
@@ -68,6 +72,9 @@ private:
     Core::Memory::Memory& memory;
     CommandGenerator command_generator;
     std::size_t elapsed_frame_count{};
+    Core::Timing::CoreTiming& core_timing;
+    std::shared_ptr<Core::Timing::EventType> process_event;
+    std::mutex lock;
 };
 
 } // namespace AudioCore
