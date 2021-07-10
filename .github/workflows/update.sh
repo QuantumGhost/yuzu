702d42abe8ac3c8fe716207217a0a9d9da3ebc30
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2021-07-10 16:19:56 -0700

    gcc-11

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-export QT_QPA_PLATFORMTHEME=gtk3
-
 zenity --question --timeout=10 --title="yuzu updater" --text="New update available. Update now?" --icon-name=yuzu --window-icon=yuzu.svg --height=80 --width=400
 answer=$?
 
 if [ "$answer" -eq 0 ]; then 
 	$APPDIR/usr/bin/AppImageUpdate $PWD/yuzu-x86_64.AppImage && $PWD/yuzu-x86_64.AppImage
 elif [ "$answer" -eq 1 ]; then
-	$APPDIR/usr/bin/yuzu
+	$APPDIR/AppRun-patched
 elif [ "$answer" -eq 5 ]; then
-	$APPDIR/usr/bin/yuzu
+	$APPDIR/AppRun-patched
 fi
 exit 0
