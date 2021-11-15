0386e477a015ab08963181c1e60485a8096ef85d
pineappleEA <pineaea@gmail.com>
2021-11-15 23:04:15 +0100

    early-access version 2203

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 yuzu emulator early access
 =============
 
-This is the source code for early-access 2202.
+This is the source code for early-access 2203.
 
 ## Legal Notice
 
--- src/core/hle/kernel/k_scheduler_lock.h
+++ src/core/hle/kernel/k_scheduler_lock.h
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include <atomic>
 #include "common/assert.h"
 #include "core/hle/kernel/k_spin_lock.h"
 #include "core/hle/kernel/k_thread.h"
@@ -20,7 +19,7 @@ public:
     explicit KAbstractSchedulerLock(KernelCore& kernel_) : kernel{kernel_} {}
 
     bool IsLockedByCurrentThread() const {
-        return owner_thread.load(std::memory_order::consume) == GetCurrentThreadPointer(kernel);
+        return owner_thread == GetCurrentThreadPointer(kernel);
     }
 
     void Lock() {
@@ -39,7 +38,7 @@ public:
 
             // Increment count, take ownership.
             lock_count = 1;
-            owner_thread.store(GetCurrentThreadPointer(kernel), std::memory_order::release);
+            owner_thread = GetCurrentThreadPointer(kernel);
         }
     }
 
@@ -54,7 +53,7 @@ public:
                 SchedulerType::UpdateHighestPriorityThreads(kernel);
 
             // Note that we no longer hold the lock, and unlock the spinlock.
-            owner_thread.store(nullptr, std::memory_order::release);
+            owner_thread = nullptr;
             spin_lock.Unlock();
 
             // Enable scheduling, and perform a rescheduling operation.
@@ -66,7 +65,7 @@ private:
     KernelCore& kernel;
     KAlignedSpinLock spin_lock{};
     s32 lock_count{};
-    std::atomic<KThread*> owner_thread{};
+    KThread* owner_thread{};
 };
 
 } // namespace Kernel
