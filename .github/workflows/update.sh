0925b6cfdb45d8413b788d10199e814b0b915681
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-04-11 20:13:36 -0700

    Pass commandline variables to yuzu

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -4,10 +4,10 @@ zenity --question --timeout=10 --title="yuzu updater" --text="New update availab
 answer=$?
 
 if [ "$answer" -eq 0 ]; then 
-	$APPDIR/usr/bin/AppImageUpdate $PWD/yuzu-x86_64.AppImage && $PWD/yuzu-x86_64.AppImage
+	$APPDIR/usr/bin/AppImageUpdate $PWD/yuzu-x86_64.AppImage && $PWD/yuzu-x86_64.AppImage "$@"
 elif [ "$answer" -eq 1 ]; then
-	$APPDIR/AppRun-patched
+	$APPDIR/AppRun-patched "$@"
 elif [ "$answer" -eq 5 ]; then
-	$APPDIR/AppRun-patched
+	$APPDIR/AppRun-patched "$@"
 fi
 exit 0
