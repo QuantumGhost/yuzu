a3a83b9639b7a37ffa049f5c7e556949b8a1c539
pineappleEA <pineaea@gmail.com>
2022-06-10 22:06:26 +0200

    early-access version 2765

--- src/common/assert.cpp
+++ src/common/assert.cpp
@@ -6,8 +6,17 @@
 
 #include "common/settings.h"
 
-void assert_handle_failure() {
-    if (Settings::values.use_debug_asserts) {
-        Crash();
+void assert_check_condition(bool cond, std::function<void()>&& on_failure) {
+    if (!cond) [[unlikely]] {
+        on_failure();
+
+        if (Settings::values.use_debug_asserts) {
+            Crash();
+        }
     }
+}
+
+[[noreturn]] void unreachable_impl() {
+    Crash();
+    throw std::runtime_error("Unreachable code");
 }
