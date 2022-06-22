dca272cb36515c547228014c28c52fba59c2af16
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2022-06-22 06:50:49 -0700

    Check directory is writeable
    Check if current directory is writeable. 
    If not, skip updating.

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -3,8 +3,15 @@
 zenity --question --timeout=10 --title="yuzu updater" --text="New update available. Update now?" --icon-name=yuzu --window-icon=yuzu.svg --height=80 --width=400
 answer=$?
 
+directory=${APPIMAGE%$ARGV0}
+
 if [ "$answer" -eq 0 ]; then 
-	$APPDIR/usr/bin/AppImageUpdate $APPIMAGE && ${APPIMAGE%$ARGV0}yuzu-x86_64.AppImage "$@"
+	if [ -w $directory ] ; then
+		$APPDIR/usr/bin/AppImageUpdate $APPIMAGE && "$directory"yuzu-x86_64.AppImage "$@"
+	else
+		echo -e "Cannot update in $directory\n\n"
+		$APPDIR/AppRun-patched "$@"
+	fi
 elif [ "$answer" -eq 1 ]; then
 	$APPDIR/AppRun-patched "$@"
 elif [ "$answer" -eq 5 ]; then
