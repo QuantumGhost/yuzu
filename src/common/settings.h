76af94908f31908c4cff1f2daf379de591815c9d
pineappleEA <pineaea@gmail.com>
2022-12-25 01:03:52 +0100

    early-access version 3247

--- src/common/settings.h
+++ src/common/settings.h
@@ -477,7 +477,7 @@ struct Values {
 
     Setting<bool> enable_raw_input{false, "enable_raw_input"};
     Setting<bool> controller_navigation{true, "controller_navigation"};
-    Setting<bool> enable_joycon_driver{false, "enable_joycon_driver"};
+    Setting<bool> enable_joycon_driver{true, "enable_joycon_driver"};
 
     SwitchableSetting<bool> vibration_enabled{true, "vibration_enabled"};
     SwitchableSetting<bool> enable_accurate_vibrations{false, "enable_accurate_vibrations"};
