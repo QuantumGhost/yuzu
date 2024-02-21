b86c4f1e6680f971f0979c0544e7e20fa1145fa3
pineappleEA <pineaea@gmail.com>
2024-02-21 02:52:05 +0100

    early-access version 4155

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 yuzu emulator early access
 =============
 
-This is the source code for early-access 4154.
+This is the source code for early-access 4155.
 
 ## Legal Notice
 
--- src/video_core/control/scheduler.h
+++ src/video_core/control/scheduler.h
@@ -3,11 +3,10 @@
 
 #pragma once
 
-#include <list>
 #include <memory>
+#include <mutex>
+#include <unordered_map>
 
-#include "common/common_types.h"
-#include "video_core/control/channel_state.h"
 #include "video_core/dma_pusher.h"
 
 namespace Tegra {
@@ -23,27 +22,13 @@ public:
     explicit Scheduler(GPU& gpu_);
     ~Scheduler();
 
-    void Init();
-
-    void Resume();
-
-    void Yield();
-
     void Push(s32 channel, CommandList&& entries);
 
     void DeclareChannel(std::shared_ptr<ChannelState> new_channel);
 
-    void ChangePriority(s32 channel_id, u32 new_priority);
-
 private:
-    void ChannelLoop(size_t gpfifo_id, s32 channel_id);
-    bool ScheduleLevel(std::list<size_t>& queue);
-    void CheckStatus();
-    bool UpdateHighestPriorityChannel();
-
-    struct SchedulerImpl;
-    std::unique_ptr<SchedulerImpl> impl;
-
+    std::unordered_map<s32, std::shared_ptr<ChannelState>> channels;
+    std::mutex scheduling_guard;
     GPU& gpu;
 };
 
